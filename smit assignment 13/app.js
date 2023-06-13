5d37c2f4492349f41640b83d1df42063e664415a
Muhammad Hussain <122493431+muhammadHussain18@users.noreply.github.com>
2023-06-13 14:22:20 -0700

    Add files via upload

--- smit assignment 13/app.js
+++ smit assignment 13/app.js
@@ -2,16 +2,20 @@ localStorage.setItem("email", "hussain@gmail.com")
 localStorage.setItem("password", "smit123")
 
 
-function submit(event) {
-    event.preventDefault();
+// console.log(getPass)
+// console.log(getEmail)
+
+
+function submit() {
     var pass = document.getElementById("exampleInputPassword1").value
     var email = document.getElementById("exampleInputEmail1").value
     var getEmail = localStorage.getItem("email")
     var getPass = localStorage.getItem("password")
-    if (email === getEmail && pass === getPass) {
-        alert("fsasafsa")
-    } else {
-        alert("invalid email")
+    if(email == getEmail && pass == getPass) {
+        console.log("welcome to quiz")
+    }
+     else {
+        console.log("incorrect information")
     }
 
 }
